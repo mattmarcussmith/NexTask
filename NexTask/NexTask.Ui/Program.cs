905104acbfeda43e211764f69db3e182af7e1bfb
Matthew Smith <matt.marcus.smith@gmail.com>
2025-03-30 18:18:48 -0500

    WIP: Update default UI project

--- NexTask/NexTask.Ui/Program.cs
+++ NexTask/NexTask.Ui/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using NexTask.UI;
+using NexTask.Ui;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
